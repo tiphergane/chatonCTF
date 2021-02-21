c244234f44aab3fd367dfdf6b7b3f618baae52d5
tiphergane <tiphergane@gmail.com>
2021-02-21 16:03:43 +0100

    correction mineures

--- chatonCTF.py
+++ chatonCTF.py
@@ -9,9 +9,7 @@ import binwalk
 def ReverseString(string):  # {{{
     reversed = string[::-1]
     return reversed
-
-
-# }}}
+    # }}}
 
 
 def FlagFinder(cible, flag):  # {{{
@@ -26,9 +24,7 @@ def FlagFinder(cible, flag):  # {{{
             pwn.success("Yeah !!!! flag found: {result}\n".format(result=a))
             pwn.warn("flag is now copied in flag.txt")
             pwn.write("flag.txt", a)
-
-
-# }}}
+    # }}}
 
 
 def binExtract(cible):  # {{{
@@ -40,66 +36,52 @@ def binExtract(cible):  # {{{
 def binScan(cible):  # {{{
     pwn.info("Scan en cours de {}".format(cible))
     binwalk.scan(cible, signature=True, extract=False)
-
-
-# }}}
+    # }}}
 
 
 def rsaFindN(p, q):  # {{{
     n = int(p) * int(q)
     return n
-
-
-# }}}
+    # }}}
 
 
 def rsaFindpq(n, x):  # {{{
     solution = int(n) // int(x)
     return solution
-
-
-# }}}
+    # }}}
 
 
 def rsaFindPhi(p, q):  # {{{
     solution = (int(p) - 1) * (int(q) - 1)
     return solution
-
-
-# }}}
+    # }}}
 
 
 def rsaFindDfromPhi(phi, e):  # {{{
     d = pow(int(e), -1, int(phi))
     return d
-
-
-# }}}
+    # }}}
 
 
 def rsaFindDfromPQ(p, q, e):  # {{{
     phi = rsaFindPhi(int(p), int(q))
     d = pow(int(e), -1, phi)
     return d
-
-
-# }}}
+    # }}}
 
 
 def rsaCipherMessage(m, e, n):  # {{{
     c = pow(int(m), int(e), int(n))
     return c
-
-
-# }}}
+    # }}}
 
 
 def rsaUncipherMessage(c, d, n):  # {{{
     m = pow(int(c), int(d), int(n))
     return m
+    # }}}
 
 
-# }}}
 if __name__ == "__main__":
     pwn.warn("Ceci est à utiliser en tant que module")
 else:
